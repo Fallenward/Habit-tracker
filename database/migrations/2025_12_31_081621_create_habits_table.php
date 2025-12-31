a98cb8c5ed9ced43c86a6e91f42a92021de64ec1
Fallenward <dontletmethink1380@gmail.com>
2025-12-31 11:51:52 +0330

    habits table

---
+++ database/migrations/2025_12_31_081621_create_habits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habits', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->jsonb('schedule')->nullable();
+            $table->timestamps();
+
+            $table->index(['user_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habits');
+    }
+};
